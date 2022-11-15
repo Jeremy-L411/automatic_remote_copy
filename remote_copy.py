4dbbbf497a62fe2c0977c92781bb2340864857f2
Jeremy <36969209+Jeremy-L411@users.noreply.github.com>
2022-11-15 10:49:35 -0700

    wait and copy change
    Adjusted wait timer to print numbers for user to see something is going on. Fixed the call for copy files, unable to use variables.

--- remote_copy.py
+++ remote_copy.py
@@ -1,35 +1,31 @@
 import os
 import time
 import sys
-import shutil
-from rich.progress import track
-
-src = "/source/to/files"
-dst = "destination/folder"
 
 
-print("connecting to VPN", file=sys.stdout)
+print("connecting to VPN, waiting 90 seconds for secure connection", file=sys.stdout)
+
+#connecting to VPN
 os.system(r'"C:\Program Files\OpenVPN\bin\openvpn-gui.exe" --command connect split_tunnel.ovpn')
-
 time.sleep(8)
 
-print("Ensuring VPN Connection, waiting 90 seconds", file=sys.stdout)
-time.sleep(90) # adjust your connection time
+
+for i in range(90):
+    print(i, file=sys.stdout)
+    time.sleep(1)
+
 
 # Mounting Drive
-os.system(r"net use W: \\server_address\TimeMachine password /user:user /persistent:no")
+os.system(r"net use W: \\NAS\Path password /user:user /persistent:no")
 
 time.sleep(8)
-
 # copying files
-os.system(r"python.exe copy_status.py {0} {1}".format(src, dst))
+os.system(r"python.exe C:\scripts\copy_status.py W:\location\of\media C:\location\to\save")
 
 # unmounting Drive
 os.system(r"net use W: /Delete")
 
 os.system(r'"C:\Program Files\OpenVPN\bin\openvpn-gui.exe" --command disconnect split_tunnel.ovpn')
 print("Disconnected from VPN", file=sys.stdout)
-
-print("Enjoy your Movies", file=sys.stdout)
 
 time.sleep(10)
